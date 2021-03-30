517a5930a8dd482f6b7420d5bf1c5134fe6fc02c
Stephen White <iamsilk3495@gmail.com>
2021-03-30 11:53:43 -0300

    Added plugin for service registrations

---
+++ SilK.Unturned.Extras/UnturnedExtrasPlugin.cs
@@ -0,0 +1,16 @@
+﻿using OpenMod.Unturned.Plugins;
+using System;
+using OpenMod.API.Plugins;
+
+[assembly: PluginMetadata("SilK.Unturned.Extras", DisplayName = "SilK's Unturned Extras",
+    Author = "SilK", Website = "https://github.com/IAmSilK/SilK.Unturned.Extras/")]
+
+namespace SilK.Unturned.Extras
+{
+    public class UnturnedExtrasPlugin : OpenModUnturnedPlugin
+    {
+        public UnturnedExtrasPlugin(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
