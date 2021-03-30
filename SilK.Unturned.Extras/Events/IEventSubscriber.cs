1cc71f48dac67728b2092ae7954e56634c9b50d1
Stephen White <iamsilk3495@gmail.com>
2021-03-30 14:50:10 -0300

    Add event subscriber docs

--- SilK.Unturned.Extras/Events/IEventSubscriber.cs
+++ SilK.Unturned.Extras/Events/IEventSubscriber.cs
@@ -3,9 +3,19 @@ using OpenMod.API.Ioc;
 
 namespace SilK.Unturned.Extras.Events
 {
+    /// <summary>
+    /// The event subscriber subscribes events to callback methods within a given class instance
+    /// based on implementations of <see cref="IExtraEventListener{TEvent}"/> and <see cref="IAsyncExtraEventListener{TEvent}"/>.
+    /// </summary>
     [Service]
     public interface IEventSubscriber
     {
+        /// <summary>
+        /// Subscribe events to callback methods within the given target class instance
+        /// which are attached to the specified <paramref name="component"/>
+        /// </summary>
+        /// <param name="target">The target class instance to subscribe events to</param>
+        /// <param name="component">The component attached to the event subscriptions</param>
         void SubscribeEvents(object target, IOpenModComponent component);
     }
 }
