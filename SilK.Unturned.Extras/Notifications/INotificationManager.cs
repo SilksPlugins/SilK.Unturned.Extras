3ccc7d6c98cbb82dd9a4a1fe5e233224d0e7e795
Stephen White <iamsilk3495@gmail.com>
2021-04-15 21:45:45 -0300

    Add notification service

---
+++ SilK.Unturned.Extras/Notifications/INotificationManager.cs
@@ -0,0 +1,21 @@
+﻿using Cysharp.Threading.Tasks;
+using OpenMod.API.Ioc;
+using OpenMod.Unturned.Users;
+
+namespace SilK.Unturned.Extras.Notifications
+{
+    /// <summary>
+    /// A service for sending notifications for players.
+    /// A basic notification implementation is provided in this plugin,
+    /// but a UI-based version is available at https://imperialplugins.com/Unturned/Products/Notifications
+    /// </summary>
+    [Service]
+    public interface INotificationManager
+    {
+        UniTask SendNotificationAsync(UnturnedUser user, string message);
+
+        UniTask SendNotificationAsync(UnturnedUser user, string message, string icon);
+
+        UniTask SendNotificationWithImageAsync(UnturnedUser user, string message, string imageUrl);
+    }
+}
---
+++ SilK.Unturned.Extras/Notifications/TextMessageNotificationManager.cs
@@ -0,0 +1,28 @@
+﻿using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using OpenMod.API.Ioc;
+using OpenMod.API.Prioritization;
+using OpenMod.Unturned.Users;
+using System.Drawing;
+
+namespace SilK.Unturned.Extras.Notifications
+{
+    [ServiceImplementation(Lifetime = ServiceLifetime.Transient, Priority = Priority.Lowest)]
+    internal class TextMessageNotificationManager : INotificationManager
+    {
+        public UniTask SendNotificationAsync(UnturnedUser user, string message)
+        {
+            return user.PrintMessageAsync(message).AsUniTask();
+        }
+
+        public UniTask SendNotificationAsync(UnturnedUser user, string message, string icon)
+        {
+            return user.PrintMessageAsync(message).AsUniTask();
+        }
+
+        public UniTask SendNotificationWithImageAsync(UnturnedUser user, string message, string imageUrl)
+        {
+            return user.PrintMessageAsync(message, Color.White, true, imageUrl).AsUniTask();
+        }
+    }
+}
