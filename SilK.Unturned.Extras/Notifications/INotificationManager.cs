6ed2ee42aa6fc97d15e38f634ced8fae1545f9a1
Stephen White <iamsilk3495@gmail.com>
2021-04-15 21:59:43 -0300

    Add docs and more notification methods

--- SilK.Unturned.Extras/Notifications/INotificationManager.cs
+++ SilK.Unturned.Extras/Notifications/INotificationManager.cs
@@ -1,6 +1,7 @@
 ﻿using Cysharp.Threading.Tasks;
 using OpenMod.API.Ioc;
 using OpenMod.Unturned.Users;
+using System.Collections.Generic;
 
 namespace SilK.Unturned.Extras.Notifications
 {
@@ -12,10 +13,70 @@ namespace SilK.Unturned.Extras.Notifications
     [Service]
     public interface INotificationManager
     {
+        /// <summary>
+        /// Send a user a notification with the given message.
+        /// </summary>
+        /// <param name="user">The user to send the notification to.</param>
+        /// <param name="message">The message to send.</param>
         UniTask SendNotificationAsync(UnturnedUser user, string message);
 
+        /// <summary>
+        /// Send a user a notification with the given message and text-based icon.
+        /// </summary>
+        /// <param name="user">The user to send the notification to.</param>
+        /// <param name="message">The message to send.</param>
+        /// <param name="icon">The text-based icon to send alongside the message.</param>
         UniTask SendNotificationAsync(UnturnedUser user, string message, string icon);
 
+        /// <summary>
+        /// Send a user a notification with the given message and an image icon.
+        /// </summary>
+        /// <param name="user">The user to send the notification to.</param>
+        /// <param name="message">The message to send.</param>
+        /// <param name="imageUrl">The url of the image icon to send alongside the message.</param>
         UniTask SendNotificationWithImageAsync(UnturnedUser user, string message, string imageUrl);
+
+        /// <summary>
+        /// Send multiple users a notification with the given message.
+        /// </summary>
+        /// <param name="users">The users to send the notification to.</param>
+        /// <param name="message">The message to send.</param>
+        UniTask SendNotificationAsync(IEnumerable<UnturnedUser> users, string message);
+
+        /// <summary>
+        /// Send multiple users a notification with the given message and text-based icon.
+        /// </summary>
+        /// <param name="users">The users to send the notification to.</param>
+        /// <param name="message">The message to send.</param>
+        /// <param name="icon">The text-based icon to send alongside the message (i.e. <c>!</c>, <c>&lt;3</c>)</param>
+        UniTask SendNotificationAsync(IEnumerable<UnturnedUser> users, string message, string icon);
+
+        /// <summary>
+        /// Send multiple users a notification with the given message and an image icon.
+        /// </summary>
+        /// <param name="users">The users to send the notification to.</param>
+        /// <param name="message">The message to send.</param>
+        /// <param name="imageUrl">The url of the image icon to send alongside the message.</param>
+        UniTask SendNotificationWithImageAsync(IEnumerable<UnturnedUser> users, string message, string imageUrl);
+
+        /// <summary>
+        /// Send all Unturned users a notification with the given message.
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        UniTask SendAllUsersNotificationAsync(string message);
+
+        /// <summary>
+        /// Send all Unturned users a notification with the given message and text-based icon.
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        /// <param name="icon">The text-based icon to send alongside the message (i.e. <c>!</c>, <c>&lt;3</c>)</param>
+        UniTask SendAllUsersNotificationAsync(string message, string icon);
+
+        /// <summary>
+        /// Send all Unturned users a notification with the given message and an image icon.
+        /// </summary>
+        /// <param name="message">The message to send.</param>
+        /// <param name="imageUrl">The url of the image icon to send alongside the message.</param>
+        UniTask SendAllUsersNotificationWithImageAsync(string message, string imageUrl);
     }
 }
--- SilK.Unturned.Extras/Notifications/TextMessageNotificationManager.cs
+++ SilK.Unturned.Extras/Notifications/TextMessageNotificationManager.cs
@@ -1,15 +1,26 @@
-﻿using Cysharp.Threading.Tasks;
+﻿extern alias JetBrainsAnnotations;
+using Cysharp.Threading.Tasks;
+using JetBrainsAnnotations::JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using OpenMod.API.Ioc;
 using OpenMod.API.Prioritization;
 using OpenMod.Unturned.Users;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace SilK.Unturned.Extras.Notifications
 {
+    [UsedImplicitly]
     [ServiceImplementation(Lifetime = ServiceLifetime.Transient, Priority = Priority.Lowest)]
     internal class TextMessageNotificationManager : INotificationManager
     {
+        private readonly IUnturnedUserDirectory _userDirectory;
+
+        public TextMessageNotificationManager(IUnturnedUserDirectory userDirectory)
+        {
+            _userDirectory = userDirectory;
+        }
+
         public UniTask SendNotificationAsync(UnturnedUser user, string message)
         {
             return user.PrintMessageAsync(message).AsUniTask();
@@ -24,5 +35,44 @@ namespace SilK.Unturned.Extras.Notifications
         {
             return user.PrintMessageAsync(message, Color.White, true, imageUrl).AsUniTask();
         }
+
+        public async UniTask SendNotificationAsync(IEnumerable<UnturnedUser> users, string message)
+        {
+            await UniTask.SwitchToMainThread();
+
+            foreach (var user in users)
+            {
+                await SendNotificationAsync(user, message);
+            }
+        }
+
+        public async UniTask SendNotificationAsync(IEnumerable<UnturnedUser> users, string message, string icon)
+        {
+            await UniTask.SwitchToMainThread();
+
+            foreach (var user in users)
+            {
+                await SendNotificationAsync(user, message, icon);
+            }
+        }
+
+        public async UniTask SendNotificationWithImageAsync(IEnumerable<UnturnedUser> users, string message, string imageUrl)
+        {
+            await UniTask.SwitchToMainThread();
+
+            foreach (var user in users)
+            {
+                await SendNotificationWithImageAsync(user, message, imageUrl);
+            }
+        }
+
+        public UniTask SendAllUsersNotificationAsync(string message) =>
+            SendNotificationAsync(_userDirectory.GetOnlineUsers(), message);
+
+        public UniTask SendAllUsersNotificationAsync(string message, string icon) =>
+            SendNotificationAsync(_userDirectory.GetOnlineUsers(), message, icon);
+
+        public UniTask SendAllUsersNotificationWithImageAsync(string message, string imageUrl) =>
+            SendNotificationWithImageAsync(_userDirectory.GetOnlineUsers(), message, imageUrl);
     }
 }
