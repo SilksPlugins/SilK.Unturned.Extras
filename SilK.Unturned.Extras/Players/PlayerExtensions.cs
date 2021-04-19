54e0a530f5334fdfcac65d0bd6547c01f0fb0aac
Stephen White <iamsilk3495@gmail.com>
2021-04-18 22:50:55 -0300

    Added some player extensions

---
+++ SilK.Unturned.Extras/Players/PlayerExtensions.cs
@@ -0,0 +1,22 @@
+﻿using OpenMod.Unturned.Players;
+using OpenMod.Unturned.Users;
+using SDG.NetTransport;
+using SDG.Unturned;
+using Steamworks;
+
+namespace SilK.Unturned.Extras.Players
+{
+    public static class PlayerExtensions
+    {
+        public static CSteamID GetSteamId(this Player player) => player.channel.owner.playerID.steamID;
+
+        public static ITransportConnection GetTransportConnection(this Player player) =>
+            player.channel.GetOwnerTransportConnection();
+
+        public static ITransportConnection GetTransportConnection(this UnturnedPlayer player) =>
+            GetTransportConnection(player.Player);
+
+        public static ITransportConnection GetTransportConnection(this UnturnedUser user) =>
+            GetTransportConnection(user.Player.Player);
+    }
+}
