8b14419b999748d91113a94475d9f934086c46d1
IAmSilK <iamsilk3495@gmail.com>
2021-07-05 15:36:32 -0300

    Improved ConfigurationParser by only creating new config instances on the reload event

--- SilK.Unturned.Extras/Configuration/ConfigurationParser.cs
+++ SilK.Unturned.Extras/Configuration/ConfigurationParser.cs
@@ -1,16 +1,50 @@
 ﻿using Microsoft.Extensions.Configuration;
+using OpenMod.API;
+using OpenMod.API.Eventing;
+using OpenMod.Core.Plugins.Events;
+using System;
+using System.Threading.Tasks;
 
 namespace SilK.Unturned.Extras.Configuration
 {
-    internal class ConfigurationParser<T> : IConfigurationParser<T> where T : class, new()
+    internal class ConfigurationParser<T> : IConfigurationParser<T>, IDisposable where T : class, new()
     {
         private readonly IConfiguration _configuration;
+        private readonly IDisposable _disposable;
 
-        public ConfigurationParser(IConfiguration configuration)
+        public ConfigurationParser(IConfiguration configuration,
+            IEventBus eventBus,
+            IOpenModComponent component)
         {
             _configuration = configuration;
+
+            _disposable = eventBus.Subscribe(component,
+                (EventCallback<PluginConfigurationChangedEvent>) OnConfigurationChangedEvent);
         }
 
-        public T Instance => _configuration.Get<T>() ?? new T();
+        private T? _instance;
+
+        private T CreateInstance()
+        {
+            return _configuration.Get<T>() ?? new T();
+        }
+
+        public T Instance => _instance ??= CreateInstance();
+
+        private Task OnConfigurationChangedEvent(IServiceProvider serviceProvider, object? sender,
+            PluginConfigurationChangedEvent @event)
+        {
+            if (_instance != null)
+            {
+                _instance = CreateInstance();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _disposable.Dispose();
+        }
     }
 }
